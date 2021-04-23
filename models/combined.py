e732264736c60b0da73124890a812c3b5102e7d6
Gabriel Rębacz <gabriel.rebacz@enigma.com.pl>
2021-04-23 18:30:48 +0200

    .

--- models/combined.py
+++ models/combined.py
@@ -6,8 +6,8 @@ class CombinedResNet(nn.Module):
 
     def __init__(self, source_model: PreActResNet_cifar, target_model: PreActResNet_cifar):
         super(CombinedResNet, self).__init__()
-        self.source_model = source_model
-        self.target_model = target_model
+        self.source_model: PreActResNet_cifar = source_model
+        self.target_model: PreActResNet_cifar = target_model
         self.alfa_source = None
         self.alfa_target = None
 
