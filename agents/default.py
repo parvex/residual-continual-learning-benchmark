4033d03d55375a2e3045786c97dc431a024c52e4
Gabriel Rębacz <gabriel.rebacz@enigma.com.pl>
2021-05-17 17:44:08 +0200

    restore 2

--- agents/default.py
+++ agents/default.py
@@ -244,10 +244,8 @@ class NormalNN(nn.Module):
         return self
 
 def accumulate_acc(output, target, task, meter):
-    if 'All' in output.keys():
-        # Single - headed model
+    if 'All' in output.keys(): # Single-headed model
         meter.update(accuracy(output['All'], target), len(target))
-
     else:  # outputs from multi-headed (multi-task) model
         for t, t_out in output.items():
             inds = [i for i in range(len(task)) if task[i] == t]  # The index of inputs that matched specific task
